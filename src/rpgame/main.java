2e32cbc2eaf3fb197f9a6b9bf5a0a35006a5dd6e
ALİ EVREN <AL EVREN@192.168.1.240>
2026-04-07 17:07:48 +0300

    Classes created, Abilities started, Difficulty with making Ability Array

--- src/rpgame/main.java
+++ src/rpgame/main.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rpgame;
 
-/**
- *
- * @author HP
- */
 public class main {
     
     public static void main(){
-        
-        
-        
+        //Stuck at creating a mage abilities arr and showing them.
     }
     
 }
