326704547c4f6073fff5b32df5d8891c56aeef06
ALİ EVREN <AL EVREN@192.168.1.240>
2026-04-07 21:40:11 +0300

    Ability array is working now :) /timespend = 3 hour (felt like 12year)

--- src/rpgame/main.java
+++ src/rpgame/main.java
@@ -3,7 +3,7 @@ package rpgame;
 public class main {
     
     public static void main(){
-        MageAbilities mageList = new MageAbilities(3);
+        ClassAbilityList mageList = new ClassAbilityList(3);
         Abilities a1 = new Abilities("Fireball" , "Throw a huge fireball to the enemy. Be carefull you may hurt your loved ones." , 7);
         Abilities a2 = new Abilities("Stun'em" , "With using electric make your opponents studder" , 3);
         Abilities a3 = new Abilities("PlaceHolder" , "This place is being hold by this ability" , 1);
