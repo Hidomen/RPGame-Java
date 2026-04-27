394a5ec36a4d0e8522c4290319b3fc4a9977ea60
Hidomen <78243964+Hidomen@users.noreply.github.com>
2026-04-27 12:45:31 +0300

    Update Item.java

--- src/rpgame/Item.java
+++ src/rpgame/Item.java
@@ -1,14 +1,37 @@
 package rpgame;
 
 public class Item {
-    protected String name;
-    protected int price;
-    protected Classes [] compability;
+    private String name;
+    private int price;
+    private Classes [] compability;
 
+    private double[] modifiers = {0, 0, 0, 0};
     
-    public Item(String name, int price, Classes[] compability) {
+    private static final int HEALTH_M_INDEX = 0;
+    private static final int ATTACK_M_INDEX = 1;
+    private static final int ABILITY_M_INDEX = 2;
+    private static final int MAXMANA_M_INDEX = 3;
+    
+    public Item(String name, int price, Classes[] compability, double[] modifiers) {
         this.name = name;
         this.price = price;
         this.compability = compability;
+        this.modifiers = modifiers;
+    }
+
+    public double getHealthModifier(){
+        return modifiers[HEALTH_M_INDEX];
+    }
+
+    public double getAttackModifier(){
+        return modifiers[ATTACK_M_INDEX];
+    }
+
+    public double getAbilityModifier(){
+        return modifiers[ABILITY_M_INDEX];
+    }
+
+    public double getMaxManaModifier(){
+        return modifiers[MAXMANA_M_INDEX];
     }
 }
